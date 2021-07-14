9defca0b4e8dadb277b36f9b7cf9d290565249cc
Silas Getachew <silogecho@gmail.com>
2021-07-14 09:55:59 +0300

    Simple API Gateway

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express")
+const app = require("express")()
+const helmet = require("helmet")
+const routes = require("./src/routes")
+const morgan = require('morgan')
+const PORT = process.env.PORT | 7171
+const axios = require('axios')
+const apiAdapter = require('./apiAxios')
+var router = express.Router()
+/**
+ * Ports to use for public IP in tiltek server
+ * 7171
+ * 7272
+ * 7373
+ */
+
+const BASE_URL = process.env.APIURL | 'http://197.156.65.6:7171'
+const api = apiAdapter(BASE_URL)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.use(helmet())
+app.use(morgan('tiny'))
+// app.use(routes)
+router.get('*', (req, res) => {
+    api.get(req.path).then(resp => {
+        res.send(resp.data)
+    })
+})
+router.post("*", (req, res, next) => {
+    api.post(req.path, req.body).then(resp => {
+        res.send(resp.data)
+    })
+})
+router.put("*", (req, res, next) => {
+    api.put(req.path, req.body).then(resp => {
+        res.send(resp.data)
+    })
+})
+router.delete("*", (req, res, next) => {
+    api.delete(req.path, req.body).then(resp => {
+        res.send(resp.data)
+    })
+})
+router.patch("*", (req, res, next) => {
+    api.patch(req.path, req.body).then(resp => {
+        res.send(resp.data)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening at port ${PORT}`)
+})
+
+
+
+module.exports = app
---
+++ config/config.js
@@ -0,0 +1,19 @@
+
+
+const MONGO_OPTOINS = {
+    minPoolSize: 1,
+    maxPoolSize: 20,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    socketTimeoutMS: 60000,
+    serverSelectionTimeoutMS: 60000,
+};
+
+console.log(process.env.MONGO_URL, 'URL');
+const MONGO = {
+    url: process.env.MONGO_URL || 'mongodb://192.168.0.10/ivr',
+    options: MONGO_OPTOINS,
+};
+
+module.exports = MONGO;
