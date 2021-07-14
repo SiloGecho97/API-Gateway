d1655d9e129c813ba63b0e48bbe50e1d8e71726b
Silas Getachew <silogecho@gmail.com>
2021-07-14 11:58:32 +0300

    Server modified

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const url = require('url')
 const express = require('express')
+const morgan = require('morgan')
+const helmet = require("helmet")
+
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
+app.use(morgan('tiny'))
+app.use(helmet())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const localServer = createProxyMiddleware({ target: 'http://197.156.65.6:7272' });
-
 app.use("/api/v1", localServer)
 
-app.listen(7272, () => { console.log('App listening on port 8000') })
+app.listen(7272, () => { console.log('App listening on port 7272') })
