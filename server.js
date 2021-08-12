0e292db2d6949024817be430840b15abb0ca4239
Silas Getachew <silogecho@gmail.com>
2021-08-12 11:32:28 +0300

    feat : change URL to env

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const url = require('url')
 const express = require('express')
 const morgan = require('morgan')
-const helmet = require("helmet")
-
+require("dotenv").config()
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const app = express()
 app.use(morgan('tiny'))
-// app.use(helmet())
 
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: true }))
 
-const localServer = createProxyMiddleware({ target: 'http://197.156.65.6:7272' });
+const localServer = createProxyMiddleware({ target: process.env.URL || 'http://192.168.0.105:7272' });
 app.use("/api/v1", localServer)
 
 app.listen(7272, () => { console.log('App listening on port 7272') })
